11d17269d00da7e6a6e9aa1367eea149c72a6dcd
Jonathan  Navarro <jonathannavarro@Jonathans-MacBook-Pro.local>
2019-05-02 20:06:56 -0700

    renamed a variable with a more appropriate one

--- soccer.py
+++ soccer.py
@@ -5,7 +5,7 @@ import re
 def smallest_difference(filename):
     lines = tuple(open(filename, "r"))
     min_difference = sys.maxsize
-    result = ""
+    end_team = ""
     FORMAT = 9
     TEAM = 1
     FOR = 6
@@ -19,9 +19,9 @@ def smallest_difference(filename):
 
             if difference < min_difference:
                 min_difference = difference
-                result = new_line[TEAM]
+                end_team = new_line[TEAM]
 
-    print("team with smallest difference: ", result)
+    print("team with smallest difference: ", end_team)
 
 
 if __name__ == "__main__":
